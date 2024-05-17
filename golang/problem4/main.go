40fabf722535c14bdfde4a4700ac80052287cac2
Muhammad Farhan Adriansyah <farhanprogrammer@gmail.com>
2024-05-17 16:47:29 +0700

    Problem 4

--- golang/problem4/main.go
+++ golang/problem4/main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	var alas float64
+	var tinggi float64
+
+	fmt.Println("Masukkan Alas:")
+	fmt.Scanln(&alas)
+	fmt.Println("Masukkan Tinggi")
+	fmt.Scanln(&tinggi)
+
+	var luas float64 = 0.5 * alas * tinggi
+
+	fmt.Println("Luas Segitiganya Adalah:", luas)
 }
