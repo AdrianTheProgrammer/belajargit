257145692e0d7e42b628ec10fd6450f15e09dd18
Muhammad Farhan Adriansyah <farhanprogrammer@gmail.com>
2024-05-17 17:14:46 +0700

    Problem 5

---
+++ golang/problem5/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var tinggi float64
+	var radius float64
+
+	fmt.Println("Masukkan Tinggi Tabung:")
+	fmt.Scanln(&tinggi)
+	fmt.Println("Masukkan Jari-Jari Tabung:")
+	fmt.Scanln(&radius)
+
+	var luas float64 = 2*3.14*radius*radius + 2*3.14*radius*tinggi
+
+	fmt.Println("Luas Permukaan Tabung Adalah:", luas)
+}
