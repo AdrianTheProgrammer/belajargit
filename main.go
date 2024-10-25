422ce749cd2dbe42de588d13bc9d1cc3566e35ff
Muhammad Farhan Adriansyah <farhanprogrammer@gmail.com>
2024-10-25 10:41:03 +0700

    anjing

--- main.go
+++ main.go
@@ -1,68 +1,37 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
+import "fmt"
+
+type Product struct {
+	Quantity int
+	SubTotal uint64
+}
 
 func main() {
-	arr := [][]int32{{52, 80}, {34, 84}}
-	arrlist := make(map[int][]int32)
-	//var arrunique []int32
-	// var unique int32
-	arrconv := make(map[int][]string)
-
-	for i := 0; i < len(arr); i++ {
-		for j := arr[i][0]; j <= arr[i][1]; j++ {
-			arrlist[i] = append(arrlist[i], j)
-		}
+	product := [3]Product{}
+	product[0] = Product{
+		Quantity: 1,
+		SubTotal: 4299000,
+	}
+	product[1] = Product{
+		Quantity: 1,
+		SubTotal: 3299000,
+	}
+	product[2] = Product{
+		Quantity: 2,
+		SubTotal: 21999000,
 	}
 
-	fmt.Println(arrlist)
+	var grandTotal uint64
 
-	for i := 0; i < len(arrlist); i++ {
-		for j := 0; j < len(arrlist[i]); j++ {
-			intconv := strconv.Itoa(int(arrlist[i][j]))
-			arrconv[i] = append(arrconv[i], intconv)
-		}
-	}
-	unique := true
-
-	for i := 0; i < len(arrconv); i++ {
-		for j := 0; j < len(arrconv[i]); j++ {
-			// fmt.Println(arrconv[i][j])
-			for k := 0; k < len(arrconv[i][j]); k++ {
-				start := 1
-
-				for l := start; l < len(arrconv[i][j]) && unique; l++ {
-					fmt.Println(string(arrconv[i][j][k]), "=", string(arrconv[i][j][l]))
-					if arrconv[i][j][k] == arrconv[i][j][l] {
-						unique = false
-					}
-				}
-
-				start++
-			}
-		}
+	for i, val := range product {
+		product[i].SubTotal = val.SubTotal * uint64(val.Quantity)
+		grandTotal += product[i].SubTotal
 	}
 
-	// fmt.Println(arrconv)
+	fmt.Println(product)
+	fmt.Println(grandTotal)
 
-	// bytes := "12344"
-	// start := 1
-	// //unique := true
-
-	// for i := 0; i < len(bytes) && unique; i++ {
-
-	// 	for j := start; j < len(bytes) && unique; j++ {
-	// 		fmt.Println(string(bytes[i]), "=", string(bytes[j]))
-	// 		if bytes[i] == bytes[j] {
-	// 			unique = false
-	// 		}
-
-	// 	}
-	// 	start++
-	// }
-
-	// fmt.Println(bytes, unique)
+	// var number Product
+	// fmt.Println(number)
 }
